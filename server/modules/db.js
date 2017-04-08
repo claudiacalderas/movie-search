e9e3a6e8d8b7948ff624099fb8795758df12dbaf
Claudia Calderas <claudia.calderas@gmail.com>
2017-04-08 18:42:18 -0500

    pointing to mLab DB

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var mongoURI = 'mongodb://localhost:27017/movieDB';
+// var mongoURI = 'mongodb://localhost:27017/movieDB';
+var mongoURI = 'mongodb://dbmaster:12345@ds155820.mlab.com:55820/moviedb';
 var MongoDB = mongoose.connect(mongoURI).connection;
 
 MongoDB.on('error', function(err){
